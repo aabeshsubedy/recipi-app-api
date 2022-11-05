e38a5d6b110b8f6cca39fe01b67cd14dc15fda8d
Aabesh Subedi <aabeshsubedy@gmail.com>
2022-11-05 22:36:12 +0545

    latest

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,10 +1,12 @@
 """
 Calculator functions
 """
-    
-def add(x,y):
+
+
+def add(x, y):
     """Add x and y and return result."""
     return x + y
+
 
 def subtract(x, y):
     """subtract x from y and return result"""
